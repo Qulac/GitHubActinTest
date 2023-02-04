251bf4d06c1b05cd58abefdf61c3bc91aa40623c
Алексей Панков <aleksej.pankow@outlook.com>
2023-02-04 17:48:20 +0300

    com

--- GitHubActinTest/Program.cs
+++ GitHubActinTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitHubActinTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("OK");
+            Console.WriteLine("OK!!!");
         }
     }
 }
